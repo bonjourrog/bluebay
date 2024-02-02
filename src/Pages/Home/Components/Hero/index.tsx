090c6d3c7805ec5c50fc5877fadde5065f662241
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-02-02 13:35:41 -0700

    feat: redesign hero section
    - change background image
    - change tagline
    - remove button and sub tagline

--- src/Pages/Home/Components/Hero/index.tsx
+++ src/Pages/Home/Components/Hero/index.tsx
@@ -5,22 +5,23 @@ const Hero = () => {
         <section className="hero">
             <div className="hero__image">
                 <img
-                    src="https://img.freepik.com/free-photo/car-road-night-city_384344-5499.jpg?t=st=1706759972~exp=1706763572~hmac=4b9234d66c0b1898f63e443c3b1765d7cb11354b1381cb5e25e433f18ac4b2ee&w=826"
+                    src="https://res.cloudinary.com/dtrkv16qb/image/upload/v1706902237/bluebay/hqk9dj9bfreduarjisyc.png"
                     alt="parked cars"
                 />
             </div>
             <div className="hero__headlines">
                 <h1 className="hero__tagline hero__headline">
-                    Unleash the Journey, <br />
-                    Embrace the Experience
+                    Ride with Confidence
+                    {/* Unleash the Journey <br />
+                    Embrace the Experience */}
                 </h1>
-                <p className="hero__introduction hero__headline">
+                {/* <p className="hero__introduction hero__headline">
                     Step into a world where your desires set the course.
-                </p>
+                </p> */}
             </div>
-            <div className="hero__button">
+            {/* <div className="hero__button">
                 <button className="button">Explore Vehicles</button>
-            </div>
+            </div> */}
         </section>
     );
 };
