be5946cb4aeabd9180e28aba6fa4b888cb604d2d
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-02-02 13:37:00 -0700

    feat: redesign navbar styles

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Header.css";
 
 const Header = () => {
     const [toggleMenu, setToggleMenu] = useState<boolean>(false);
+    const [scrolling, setScrolling] = useState<boolean>(false);
+    const [scroll, setScroll] = useState<number>(0);
+
+    const handleScroll = () => {
+        setScrolling(window.scrollY > 6);
+        setScroll(window.scrollY);
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <header className="header">
-            <nav className="nav">
-                <span className="logo">BLUEBAY</span>
+            <nav
+                style={{
+                    top: scrolling ? "1.5em" : "0em",
+                    width: scrolling ? "97%" : "100%",
+                    borderRadius: scrolling ? "1em" : "0em",
+                    height: scrolling ? "4em" : "6em",
+                    backgroundColor: scrolling
+                        ? "rgba(255, 255, 255, 0.96)"
+                        : "rgba(255, 255, 255, 0.901)",
+                }}
+                className="nav"
+            >
+                <p
+                    style={{ fontSize: scrolling ? "1em" : "2em" }}
+                    className="logo"
+                >
+                    BLUEBAY
+                </p>
                 <span
                     onClick={() => setToggleMenu(!toggleMenu)}
                     className="menu"
@@ -19,11 +49,21 @@ const Header = () => {
                         toggleMenu ? "menu__opened" : "menu__closed"
                     }`}
                 >
-                    <li>About</li>
-                    <li>Location</li>
+                    <li
+                        style={{
+                            fontWeight: scroll > 490 ? "700" : "400",
+                            letterSpacing: scroll > 490 ? ".1em" : "0em",
+                            borderBottom:
+                                scroll > 490
+                                    ? ".2em solid rgb(72, 95, 200)"
+                                    : ".2em solid transparent",
+                        }}
+                    >
+                        Fleet
+                    </li>
+                    <li>Services</li>
                     <li>Contact</li>
                     <li>carwash</li>
-                    <li>cars</li>
                 </ul>
             </nav>
         </header>
