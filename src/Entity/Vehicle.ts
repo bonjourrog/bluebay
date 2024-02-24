12fa20c72dd2d1ca433a19c385fb30fb087bc366
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-02-23 21:46:43 -0700

    chore: add new field into Vehicle entity

--- src/Entity/Vehicle.ts
+++ src/Entity/Vehicle.ts
@@ -1,23 +1,34 @@
 export interface Vehicle {
+    id:string;
     make: string;         // Marca del vehículo
     model: string;        // Modelo del vehículo
     year: number;         // Año de fabricación
     color: string;        // Color del vehículo
-    fuelType: string;     // Tipo de combustible (gasolina, diésel, híbrido, etc.)
+    fuel_type: string;     // Tipo de combustible (gasolina, diésel, híbrido, etc.)
     transmission: string; // Tipo de transmisión (automática, manual)
-    passengerCapacity: number; // Capacidad de pasajeros
+    passenger_capacity: number; // Capacidad de pasajeros
     doors: number;
     sport:boolean;
-    dailyRate: number;    // Tarifa diaria de alquiler
+    daily_rate: number;    // Tarifa diaria de alquiler
     image: string;
     type: CarType;
     available: boolean;   // Disponibilidad actual
+    steering_wheel_controls:boolean;
+    anti_glare_tint: boolean;
+    auto_stick: boolean;
+    amount: number;
+    deposit:number;
+    license: string;
+    touch_screen:boolean;
+    images:string[];
+    reverse_camera:boolean;
+
 } 
 
 export enum CarType {
-    Small = "chico",
-    Medium = "medianos",
-    Large = "grandes",
+    Small = "small",
+    Medium = "medium",
+    Large = "large",
 }
 
 export interface ElectricVehicle extends Vehicle {
