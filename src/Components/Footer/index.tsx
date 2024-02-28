9d87599cfc7800cf2ae8d17e25ba00ce6adab633
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-02-28 08:48:39 -0700

    feat: implement condicional bg color into footer

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -2,12 +2,13 @@ import "./Footer.css";
 import { FaFacebook } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { IoLogoWhatsapp } from "react-icons/io";
+import { FooterProps } from "./Footer.props";
 
-const Footer = () => {
+const Footer:React.FC<FooterProps> = ({DarkBg}) => {
     return (
-        <footer className="Footer">
+        <footer className="Footer" style={{backgroundColor:DarkBg?'var(--app-gray)':'white'}}>
             <p className="Footer__logo">BLUEBAY</p>
-            <p className="Footer__message">Siguenos en redes sociales</p>
+            <p className="Footer__message" style={{color:DarkBg?'white':'var(--app-gray)'}}>Siguenos en redes sociales</p>
             <div className="Footer__socials">
                 <a target="_blank" href="https://www.facebook.com/profile.php?id=61555585719547">
                     <FaFacebook className="business__social-icon" />
