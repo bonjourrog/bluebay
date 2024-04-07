64cce9aebe8c98ce68dd1ddfbaa5a04213e5071e
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-04-07 11:41:47 -0700

    style: add styles to open hpur section from footer

--- src/Components/Footer/OpenHours/index.tsx
+++ src/Components/Footer/OpenHours/index.tsx
@@ -2,13 +2,13 @@ import './OpenHours.css';
 import { OpenHoursProps } from './OpenHours.props';
 
 const OpenHours: React.FC<OpenHoursProps> = ()=>{
-    return <ul>
-        <li>Lunes.....08:00-07:00</li>
-        <li>Martes.....08:00-07:00</li>
-        <li>Miercoles.....08:00-07:00</li>
-        <li>Jueves.....08:00-07:00</li>
-        <li>Viernes.....08:00-07:00</li>
-        <li>Sabado.....08:00-07:00</li>
+    return <ul className='openhours'>
+        <li className='openhours__time'><p className='openhours__day'>Lunes</p>.............08:00-07:00</li>
+        <li className='openhours__time'><p className='openhours__day'>Martes</p>............08:00-07:00</li>
+        <li className='openhours__time'><p className='openhours__day'>Miercoles</p>.....08:00-07:00</li>
+        <li className='openhours__time'><p className='openhours__day'>Jueves</p>............08:00-07:00</li>
+        <li className='openhours__time'><p className='openhours__day'>Viernes</p>...........08:00-07:00</li>
+        <li className='openhours__time'><p className='openhours__day'>Sabado</p>...........08:00-07:00</li>
     </ul>
 }
 export default OpenHours;
